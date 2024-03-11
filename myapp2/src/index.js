c5a42fcc128d992d3e0fb9a7d0716d818d81a5c7
Jahnavi Cse <158044331+jahnavicse@users.noreply.github.com>
2024-03-11 10:46:29 +0000

    Childa

---
+++ myapp2/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App13a from "./App13a";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <App13a />
+  </>
+);
--- src/App2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function App2() {
-  return (
-    <div>Hello World</div>
-  )
-}
-
-
